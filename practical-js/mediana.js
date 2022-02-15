88ea69000e0d7d6b2b9d851b26de9019d7fb1f45
nicovb <giraldojnicolas@gmail.com>
2022-02-15 11:44:53 -0500

    median

---
+++ practical-js/mediana.js
@@ -0,0 +1,43 @@
+function calculateMean (list) {
+    let sum1 = list.reduce(
+        function (pValue = 0, nValue) {
+            return pValue + nValue;
+        });
+const meanOfTheList = sum1 / list.length;
+return meanOfTheList;
+}
+
+const list1 = [];
+
+function addNumber () {
+    const addNumber1 = document.getElementById("inputNumber");
+    const number1 = parseInt(addNumber1.value);
+    list1.push(number1);
+    printList.innerText = list1;   
+}
+
+function isEven (number) {
+    if (number % 2 == 0) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+let median1;
+
+function calculateMedian (){
+    const halfOfTheList = parseInt(list1.length / 2);
+    if (isEven(list1.length)) {
+       const posi1 = list1[halfOfTheList - 1];
+       const posi2 = list1[halfOfTheList];
+       const mean12 = calculateMean([posi1, posi2]);
+       median1 = mean12;
+       printMedian.innerText = median1;   
+
+   }
+   else {
+       median1 = list1[halfOfTheList];
+       printMedian.innerText = median1;   
+   }
+}
