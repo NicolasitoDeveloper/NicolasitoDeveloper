a35f45bbe73f8c2195f261fff2230408ebef7614
nicovb <giraldojnicolas@gmail.com>
2022-02-15 10:39:45 -0500

    minor adjustments

--- practical-js/promedio.js
+++ practical-js/promedio.js
@@ -1,12 +1,22 @@
-function calculateMean (list) {
+const list1 = [];
+const printL = document.getElementById("printList");
+const printM = document.getElementById("printMean");
 
-const sumOfTheList = list.reduce(
-    function (accumulatedValue = 0, currentValue) {
-        return accumulatedValue + currentValue;
-    }
-);
+function addNumber () {
+    const addNumber1 = document.getElementById("inputNumber");
+    const number1 = parseInt(addNumber1.value);
+    list1.push(number1);
+    printL.innerText = list1;   
+}
 
-const meanOfTheList = sumOfTheList / list.length;
-
+function calculateMean () {
+    let sum1 = 0;
+    for ( let i = 0; i < list1.length; i++) {
+            sum1 += list1[i];
+         }
+    
+let meanOfTheList = sum1 / list1.length;
+printM.innerText = "The mean from the numbers above is: " + meanOfTheList;   
 return meanOfTheList;
+
 }
