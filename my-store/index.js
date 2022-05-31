6092510ccba230f213a4e1d1b4920edb234cf6c0
NicoVulcan <giraldojnicolas@gmail.com>
2022-05-31 18:46:57 -0500

    middlewares

--- my-store/index.js
+++ my-store/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { routerApi } = require("./routes");
+const { logErrors, errorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
 const port = 3009;
@@ -10,45 +11,10 @@ app.get("/", (req, res) => {
   res.send("Hi, I'm a server in express");
 });
 
-// app.get("/new-route", (req, res) => {
-//   res.send("Hi, I'm a new endpoint");
-// });
-
 routerApi(app);
 
-// app.get("/categories/electronics/:id", (req, res) => {
-//   const { id } = req.params;
-//   res.json({
-//     id,
-//     name: "product 1",
-//     price: 1000
-//   });
-// });
-
-// //localhost:3005/users?limit=10&offset=200
-// // {
-// //   "limit": "10",
-// //   "offset": "200"
-// // }
-// app.get("/users", (req, res) =>{
-//   const { limit, offset } = req.query;
-//   if (limit && offset) {
-//     res.json({
-//       limit,
-//       offset
-//     });
-//   } else {
-//     res.send("No params");
-//   }
-// })
-
-// app.get("/categories/:categoryId/products/:productId", (req, res) => {
-//   const { categoryId, productId } = req.params;
-//   res.json({
-//     categoryId,
-//     productId,
-//   });
-// });
+app.use(logErrors);
+app.use(errorHandler);
 
 app.listen(port, () => {
   console.log("My port: " + port);
--- my-store/routes/productsRoutes.js
+++ my-store/routes/productsRoutes.js
@@ -13,10 +13,15 @@ router.get("/filter", (req, res) => {
   res.send("I'm a filter");
 });
 
-router.get("/:id", async (req, res) => {
-  const { id } = req.params;
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
   const product = await service.findOne(id);
   res.json(product);
+  } catch (error) {
+    next(error);
+  }
+
 });
 
 router.post("/", async (req, res) => {
