a65f04711b3b588a2042f6c2b2da41c8e6c68148
NicoVulcan <giraldojnicolas@gmail.com>
2022-06-02 17:08:38 -0500

    frontend cors

--- my-store/index.js
+++ my-store/index.js
@@ -1,5 +1,7 @@
 const express = require("express");
-const { routerApi } = require("./routes");
+const cors = require('cors');
+const {routerApi} = require("./routes");
+
 const { logErrors, errorHandler, boomErrorHandler } = require("./middlewares/errorHandler");
 
 const app = express();
@@ -7,11 +9,24 @@ const port = 3009;
 
 app.use(express.json());
 
+const whitelist = ["http://localhost:3009", "https://myapp.co"];
+const options = {
+  origin: (origin, callback) => {
+    if (whitelist.includes(origin) || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not!!!"), false);
+    }
+  }
+}
+app.use(cors(options));
+routerApi(app);
+
 app.get("/", (req, res) => {
   res.send("Hi, I'm a server in express");
 });
 
-routerApi(app);
+
 app.use(boomErrorHandler);
 app.use(logErrors);
 app.use(errorHandler);
